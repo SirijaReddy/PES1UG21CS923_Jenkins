e2f1526eaddfe1c2cb50634d384990af604eab13
Sirija R <98957052+SirijaReddy@users.noreply.github.com>
2024-03-07 12:43:57 +0530

    Update hello2.cpp

--- hello2.cpp
+++ hello2.cpp
@@ -1,9 +1,9 @@
-#include<iostream.h>
-
+//File: hello.cpp
+#include <iostream>
 using namespace std;
-int main(void)
-  cout<<"In hello"<<endl;
-  cout<<"File created"<<endl;
-    return 0;
 
+int main(void) {
+  cout << "In Hello!\n";
+  cout << "Built a new file\n";
+  return 0;
 }
